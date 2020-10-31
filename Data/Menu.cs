ea0d1dd19356bd0d00ab8bb17e566b6941d427ff
Caleb Rosebaugh <rosebaugh@ksu.edu>
2020-10-30 23:42:32 -0500

    Finished Milestone

--- Data/Menu.cs
+++ Data/Menu.cs
@@ -51,25 +51,39 @@ namespace BleakwindBuffet.Data
         public static IEnumerable<IOrderItem> Sides()
         {
             List<IOrderItem> sides = new List<IOrderItem>();
-            
-            foreach(Size s in Enum.GetValues(typeof(Size)))
+            for(int i = 0; i < 4; i++)
             {
-                sides.Add(new DragonbornWaffleFries()
+                foreach(Size s in Enum.GetValues(typeof(Size)))
                 {
-                    Size = s
-                });
-                sides.Add(new FriedMiraak()
-                {
-                    Size = s
-                });
-                sides.Add(new MadOtarGrits()
-                {
-                    Size = s
-                });
-                sides.Add(new VokunSalad()
-                {
-                    Size = s
-                });
+                    switch (i)
+                    {
+                        case 0:
+                            sides.Add(new DragonbornWaffleFries()
+                            {
+                                Size = s
+                            });
+                            break;
+                        case 1:
+                            sides.Add(new FriedMiraak()
+                            {
+                                Size = s
+                            });
+                            break;
+                        case 2:
+                            sides.Add(new MadOtarGrits()
+                            {
+                                Size = s
+                            });
+                            break;
+                        case 3:
+                            sides.Add(new VokunSalad()
+                            {
+                                Size = s
+                            });
+                            break;
+                    }
+                }
+
             }
 
             return sides;
@@ -84,40 +98,67 @@ namespace BleakwindBuffet.Data
         {
             List<IOrderItem> drinks = new List<IOrderItem>();
 
-            foreach (Size s in Enum.GetValues(typeof(Size)))
+            int k = 0;
+            int j = 5;
+            SodaFlavor last = Enum.GetValues(typeof(SodaFlavor)).Cast<SodaFlavor>().Max();
+            foreach (SodaFlavor f in Enum.GetValues(typeof(SodaFlavor)))
             {
-                drinks.Add(new AretinoAppleJuice()
+                if (f == last)
                 {
-                    Size = s
-                });
-                drinks.Add(new CandlehearthCoffee()
-                {
-                    Size = s,
-                    Decaf = false
-                });
-                drinks.Add(new CandlehearthCoffee()
-                {
-                    Size = s,
-                    Decaf = true
-                });
-                drinks.Add(new MarkarthMilk()
-                {
-                    Size = s
-                });
-                foreach(SodaFlavor f in Enum.GetValues(typeof(SodaFlavor)))
-                {
-                    drinks.Add(new SailorSoda()
-                    {
-                        Size = s,
-                        Flavor = f
-                    });
+                    k = 5;
+                    j = 6;
                 }
-                drinks.Add(new WarriorWater()
+                for (int i = k; i < j; i++)
                 {
-                    Size = s
-                });
+                    foreach (Size s in Enum.GetValues(typeof(Size)))
+                    {
+                        switch (i)
+                        {
+                            case 0:
+                                drinks.Add(new AretinoAppleJuice()
+                                {
+                                    Size = s
+                                });
+                                break;
+                            case 1:
+                                drinks.Add(new CandlehearthCoffee()
+                                {
+                                    Size = s,
+                                    Decaf = false
+                                });
+                                break;
+                            case 2:
+                                drinks.Add(new CandlehearthCoffee()
+                                {
+                                    Size = s,
+                                    Decaf = true
+                                });
+                                break;
+                            case 3:
+                                drinks.Add(new MarkarthMilk()
+                                {
+                                    Size = s
+                                });
+                                break;
+                            case 4:
+                                drinks.Add(new SailorSoda()
+                                {
+                                    Size = s,
+                                    Flavor = f
+                                });
+                                break;
+                            case 5:
+                                drinks.Add(new WarriorWater()
+                                {
+                                    Size = s
+                                });
+                            break;
+                        }
+                    }
+                }
+                k = 4;
+                j = 5;
             }
-
             return drinks;
         }
     }
